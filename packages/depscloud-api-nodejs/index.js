f94a7262850172490b8d5dc0b99269533e6c4022
Mya Pitzeruse <j.mya.pitz@gmail.com>
2020-09-02 23:18:34 -0500

    gh-69: generated code

--- packages/depscloud-api-nodejs/index.js
+++ packages/depscloud-api-nodejs/index.js
@@ -4,11 +4,15 @@ const grpc = require('@grpc/grpc-js');
 
 const filenames = [];
 
-filenames.push(path.join(__dirname, "depscloud_api", "v1alpha", "tracker", "tracker.proto"));
+filenames.push(path.join(__dirname, "depscloud", "api", "v1beta", "core.proto"));
+filenames.push(path.join(__dirname, "depscloud", "api", "v1beta", "manifest.proto"));
+filenames.push(path.join(__dirname, "depscloud", "api", "v1beta", "rpc.proto"));
+filenames.push(path.join(__dirname, "depscloud_api", "v1alpha", "deps", "deps.proto"));
 filenames.push(path.join(__dirname, "depscloud_api", "v1alpha", "extractor", "extractor.proto"));
 filenames.push(path.join(__dirname, "depscloud_api", "v1alpha", "schema", "schema.proto"));
-filenames.push(path.join(__dirname, "depscloud_api", "v1alpha", "deps", "deps.proto"));
 filenames.push(path.join(__dirname, "depscloud_api", "v1alpha", "store", "store.proto"));
+filenames.push(path.join(__dirname, "depscloud_api", "v1alpha", "tracker", "tracker.proto"));
+filenames.push(path.join(__dirname, "graphstore", "api", "v1beta", "graphstore.proto"));
 
 const packageDefinition = protoLoader.loadSync(
     filenames,
